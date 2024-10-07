28549f8730244bb34527a0a2025c9649e2fb4ddf
Aditya Chandra <aditchndr44@gmail.com>
2024-10-07 14:38:33 +0700

    chore: added config merge

--- src/ParanoiaServiceProvider.php
+++ src/ParanoiaServiceProvider.php
@@ -12,10 +12,16 @@ class ParanoiaServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->register(EventServiceProvider::class);
+
+        $this->mergeConfigFrom(__DIR__ . '/../config/paranoia.php', 'paranoia');
     }
 
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/paranoia.php' => config_path('paranoia.php'),
+            ], 'config');
+        }
     }
 }
